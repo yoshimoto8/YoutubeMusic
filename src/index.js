e1236e7e916d3f69496e44172777802ab4584719
吉本光輝 <>
2018-05-11 11:45:41 +0900

    add redux saga

--- src/index.js
+++ src/index.js
@@ -2,7 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Main from "./container/Main";
+import createSagaMiddleware from "redux-saga";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
+import rootSaga from "./sagas";
 import registerServiceWorker from "./registerServiceWorker";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-ReactDOM.render(<Main />, document.getElementById("root"));
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
+
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Main />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
---
+++ src/sagas/index.js
@@ -0,0 +1,5 @@
+function* rootSaga() {
+  yield [];
+}
+
+export default rootSaga;
