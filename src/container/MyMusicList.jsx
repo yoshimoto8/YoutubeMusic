7eb934eec5599460ad5e8c9be96c0a1a3d4d5725
吉本光輝 <>
2018-05-11 14:41:21 +0900

    add Link

--- src/container/MyMusicList.jsx
+++ src/container/MyMusicList.jsx
@@ -1,19 +1,15 @@
 import React from "react";
-import AmeirImg from "../images/aimer.jpeg";
 import { connect } from "react-redux";
 import { setPlayList } from "../actions/index";
+import { Link } from "react-router-dom";
+import { Aimer, CharliePuth } from "./demoMusicList";
+import "./styles/MyMusicList.css";
 
 class MyMusicList extends React.Component {
   constructor() {
     super();
     this.state = {
-      defaultMusic: [
-        {
-          playListImg: AmeirImg,
-          playListName: "Aimer",
-          musciList: musicList1
-        }
-      ]
+      defaultMusic: [Aimer, CharliePuth]
     };
   }
 
@@ -21,73 +17,38 @@ class MyMusicList extends React.Component {
     const { defaultMusic } = this.state;
     return (
       <div className="main">
-        {defaultMusic.map((data, index) => {
-          const { playListImg, playListName } = data;
-          return (
-            <div
-              key={index}
-              className="musicPlayBox"
-              onClick={() => {
-                this.props.setPlayList(data.musciList);
-              }}
-            >
-              <img src={playListImg} alt="" />
-              <div>{playListName}</div>
-            </div>
-          );
-        })}
+        <div className="defaultMusicPlayBox">
+          <h2>Default Music</h2>
+          <div className="defaultMusicPlayList">
+            {defaultMusic.map((data, index) => {
+              const { playListImg, playListName } = data;
+              return (
+                <Link to="/">
+                  <div
+                    key={index}
+                    className="musicPlayBox"
+                    onClick={() => {
+                      this.props.setPlayList(data.musciList);
+                    }}
+                  >
+                    <img src={playListImg} alt="" height="200" width="250" />
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-const musicList1 = [
-  {
-    id: 1,
-    name: "After Dark",
-    artists: "Aimer",
-    time: 278,
-    src: "https://www.youtube.com/watch?v=hIQeXJxWMi4"
-  },
-  {
-    id: 2,
-    name: "Aimer Premier Live 2012.06.08 DIGEST",
-    artists: "Aimer",
-    time: 478,
-    src: "https://www.youtube.com/watch?v=zxvLA7xepGA"
-  },
-  {
-    id: 3,
-    name: "Re:pray",
-    artists: "Aimer",
-    time: 317,
-    src: "https://www.youtube.com/watch?v=cPB-ijSzEMk"
-  },
-  {
-    id: 4,
-    name: "六等星の夜",
-    artists: "Aimer",
-    time: 341,
-    src: "https://www.youtube.com/watch?v=jgSyul7n-8M"
-  },
-  {
-    id: 5,
-    name: "Kataomoi",
-    artists: "Aimer",
-    time: 600,
-    src: "https://www.youtube.com/watch?v=2H36K1Hi72s"
-  },
-  {
-    id: 6,
-    name: "ONE",
-    artists: "Aimer",
-    time: 341,
-    src: "https://www.youtube.com/watch?v=GOurhX0YAPQ"
-  }
-];
-
 const mapDispatchToProps = dispatch => ({
   setPlayList: defaultMusic => dispatch(setPlayList(defaultMusic))
 });
 
-export default connect(null, mapDispatchToProps)(MyMusicList);
+const mapStateToProps = state => ({
+  history: state.routing
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(MyMusicList);
---
+++ src/container/demoMusicList.js
@@ -0,0 +1,102 @@
+import AmeirImg from "../images/aimer.jpeg";
+import CharliePuthImg from "../images/charliePuth.jpeg";
+
+export const Aimer = {
+  playListImg: AmeirImg,
+  playListName: "Aimer",
+  musciList: [
+    {
+      id: 1,
+      name: "After Dark",
+      artists: "Aimer",
+      time: 278,
+      src: "https://www.youtube.com/watch?v=hIQeXJxWMi4"
+    },
+    {
+      id: 2,
+      name: "Aimer Premier Live 2012.06.08 DIGEST",
+      artists: "Aimer",
+      time: 478,
+      src: "https://www.youtube.com/watch?v=zxvLA7xepGA"
+    },
+    {
+      id: 3,
+      name: "Re:pray",
+      artists: "Aimer",
+      time: 317,
+      src: "https://www.youtube.com/watch?v=cPB-ijSzEMk"
+    },
+    {
+      id: 4,
+      name: "六等星の夜",
+      artists: "Aimer",
+      time: 341,
+      src: "https://www.youtube.com/watch?v=jgSyul7n-8M"
+    },
+    {
+      id: 5,
+      name: "Kataomoi",
+      artists: "Aimer",
+      time: 600,
+      src: "https://www.youtube.com/watch?v=2H36K1Hi72s"
+    },
+    {
+      id: 6,
+      name: "ONE",
+      artists: "Aimer",
+      time: 341,
+      src: "https://www.youtube.com/watch?v=GOurhX0YAPQ"
+    }
+  ]
+};
+
+export const CharliePuth = {
+  playListImg: CharliePuthImg,
+  playListName: "Charlie PuthImg",
+  musciList: [
+    {
+      id: 1,
+      name: "How Long",
+      artists: "Charlie PuthImg",
+      time: 330,
+      src: "https://www.youtube.com/watch?v=CwfoyVa980U"
+    },
+    {
+      id: 2,
+      name: "We Don't Talk Anymore ",
+      artists: "Charlie PuthImg",
+      time: 350,
+      src: "https://www.youtube.com/watch?v=3AtDnEC4zak"
+    },
+    {
+      id: 3,
+      name: "One Call Away",
+      artists: "Charlie PuthImg",
+      time: 361,
+      src: "https://www.youtube.com/watch?v=BxuY9FET9Y4"
+    },
+    {
+      id: 4,
+      name: "Marvin Gaye ft. Meghan Trainor",
+      artists: "Charlie PuthImg",
+      time: 328,
+      src:
+        "https://www.youtube.com/watch?v=igNVdlXhKcI&index=7&list=RDBxuY9FET9Y4"
+    },
+    {
+      id: 5,
+      name: "See You Again ft",
+      artists: "Charlie PuthImg",
+      time: 357,
+      src: "https://www.youtube.com/watch?v=RgKAFK5djSk"
+    },
+    {
+      id: 6,
+      name: "Dangerously",
+      artists: "Charlie PuthImg",
+      time: 322,
+      src:
+        "https://www.youtube.com/watch?v=TBXQu8ORnBQ&index=5&list=RDBxuY9FET9Y4"
+    }
+  ]
+};
