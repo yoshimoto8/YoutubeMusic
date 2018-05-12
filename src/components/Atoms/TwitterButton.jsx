caf6a4920ea24c1874062ae9e723b2bae50b819a
吉本光輝 <>
2018-05-12 12:55:44 +0900

    commit

---
+++ src/components/Atoms/TwitterButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/TwitterButton.css";
+
+const TwitterButton = () => {
+  return (
+    <a className="fl_tw2">
+      <i classNmae="fa fa-twitter" /> <span>Twitter ログイン</span>
+    </a>
+  );
+};
+
+export default TwitterButton;
