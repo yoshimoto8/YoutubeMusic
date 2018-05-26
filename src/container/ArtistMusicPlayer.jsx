ec236da8f996cb6db949038b2a67c50c47ca0bbd
吉本光輝 <yoshi@yoshimotomitsuterunoMacBook-puro.local>
2018-05-26 17:47:30 +0900

    commit

--- src/container/ArtistMusicPlayer.jsx
+++ src/container/ArtistMusicPlayer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import $ from "jquery";
 import "./styles/ArtistMusicPlayer.css";
 
 class ArtistMusicPlayer extends React.Component {
@@ -10,14 +11,33 @@ class ArtistMusicPlayer extends React.Component {
     };
   }
 
-  componentDidMount() {
-    this.setState({ artist: this.props.artist });
+  componentWillMount() {
+    // console.log(!sessionStorage.getItem("artist"));
+    // console.log(!!sessionStorage.getItem("artist"));
+    // if (!!sessionStorage.getItem("artist")) {
+    //   console.log("呼ばれた");
+    //   const artist = JSON.stringify(this.props.artist);
+    //   sessionStorage.setItem("artist", artist);
+    //   this.setState({ artist: this.props.artist });
+    // } else {
+    //   console.log("呼ばれた2");
+    //   const artist = sessionStorage.getItem("artist");
+    //   const objArtist = $.parseJSON(artist);
+    //   this.setState({ objArtist });
+    // }
   }
 
   render() {
     const { artist } = this.state;
-    console.log(artist);
-    return <div className="main" />;
+    const musicLength = artist.musicList.length;
+    return (
+      <div className="main">
+        <div className="ArtistMusicPlayer-artistInfo">
+          <img src={artist.src} alt="" />
+          <h2>{`${artist.name}のミュージックリスト`}</h2>
+        </div>
+      </div>
+    );
   }
 }
 
