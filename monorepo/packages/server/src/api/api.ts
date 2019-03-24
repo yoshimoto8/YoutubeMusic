ea5251b3484661f6669c7a23564e040a0ea5b797
yoshimoto8 <yoyoyo427435@gmail.com>
2019-03-25 07:40:38 +0900

    deployできるまで

--- monorepo/packages/server/src/api/api.ts
+++ monorepo/packages/server/src/api/api.ts
@@ -1,9 +1,9 @@
 import * as express from 'express'
-import { CounterResult } from '@firebase-monorepo/api-schema'
+// import { CounterResult } from 'music-api-schema'
 const app = express()
-const result = new CounterResult()
+// const result = new CounterResult()
 app.get('/', (req, res) => {
-  res.send('' + result.count)
+  res.send('' + '')
 })
 
 export function test() {
